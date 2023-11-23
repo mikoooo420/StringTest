7970f966e722be7c831f669084970b127a44442c
mikoooo420 <mikoj0314@gmail.com>
2023-11-23 13:27:02 +0100

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner skaner = new Scanner(System.in);
+        System.out.println("Podaj slowo");
+        String str = skaner.nextLine();
 
+        if (str.length() <= 2) {
+            System.out.println("Błąd");
+        } else {
+            int strprzemiana = str.length() / 2;
+
+            String str1 = str.substring(strprzemiana, strprzemiana+2);
+            String str2 = str.concat(str1);
+            System.out.println(str2);
+        }
 
     }
 }
